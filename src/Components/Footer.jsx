8f8bc20d784c051b53a7db2d436b1c756a856f30
Gull-Sher <mobilegull73@gmail.com>
2023-08-04 16:28:15 +0500

    aaa

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -21,26 +21,28 @@ const Footer3a = () => {
                     {/* Start of row div */}
 
                     <div className="col-md-4 col-sm-6 col-12">
-                        <a href="www.google.com">
-                            <img src={Logo1} id="footer_logo" alt="logo" />
+                        {/* <a href="www.google.com"> */}
 
-                        </a>
+                        <img src={Logo1} id="footer_logo" alt="logo" />
+
+                        {/* </a> */}
                         <p className="mt-20">Welcome to the Waheed Group of Companies! What started off over four decades
                             ago as a small business has today spread out by leaps and bounds to be-come Pakistan’s
                             largest business group. </p>
 
                         <ul className="social-links-footer">
                             <li>
-                                <a href="http://www.facebook.com/" target="_blank" rel="noopener noreferrer">
-                                    <i className="fa fa-facebook"></i></a>
+                                {/* <a href="http://www.facebook.com/" target="_blank" rel="noopener noreferrer">
+                                    <i className="fa fa-facebook"></i></a> */}
                             </li>
                             <li>
-                                <a href="http://www.twitter.com/" target="_blank" rel="noopener noreferrer">
-                                    <i className="fa fa-twitter"></i></a>
+                                {/* <a href="http://www.twitter.com/" target="_blank" rel="noopener noreferrer">
+                                    <i className="fa fa-twitter"></i></a> */}
                             </li>
                             <li>
-                                <a href="http://www.skype.com/" target="_blank" rel="noopener noreferrer">
-                                    <i className="fa fa-skype"></i></a></li>
+                                {/* <a href="http://www.skype.com/" target="_blank" rel="noopener noreferrer">
+                                    <i className="fa fa-skype"></i></a> */}
+                            </li>
                         </ul>
 
                     </div>
@@ -50,17 +52,28 @@ const Footer3a = () => {
 
                         <ul className="footer-news mt-25">
 
-                            <li><a href="latestNews.html#Mr Abdul Waheed, Chairman PVMA KN News">Mr Abdul Waheed, Chairman PVMA KN News</a><strong><i class="fa fa-calendar"></i> 2020-10-19</strong></li>
+                            <li>
+                                {/* <a href="latestNews.html#Mr Abdul Waheed, Chairman PVMA KN News"> */}
+                                Mr Abdul Waheed, Chairman PVMA KN News
 
-
-
+                                {/* </a> */}
+                                <strong>
+                                    <i class="fa fa-calendar"></i> 2020-10-19
+                                </strong>
+                            </li>
 
                             <li>
-                                <a href="latestNews.html#Mr. Abdul Waheed Sheikh - welcoming Guest of Honor Mr. Faisal Javed Khan">Mr.
-                                    Abdul Waheed Sheikh - welcoming Guest of Honor Mr. Faisal Javed Khan</a><strong><i
-                                        className="fa fa-calendar"></i> 2019-06-20</strong></li>
-                            <li><a href="latestNews.html#Mr. Abdul Waheed Sheikh - welcoming Ambassador of UAE">Mr.
-                                Abdul Waheed Sheikh - welcoming Ambassador of UAE</a><strong><i
+
+                                {/* <a href="latestNews.html#Mr. Abdul Waheed Sheikh - welcoming Guest of Honor Mr. Faisal Javed Khan"> */}
+                                Mr. Abdul Waheed Sheikh - welcoming Guest of Honor Mr. Faisal Javed Khan
+                                {/*  </a> */}
+                                <strong><i
+                                    className="fa fa-calendar"></i> 2019-06-20</strong></li>
+                            <li>
+                                {/* <a href="latestNews.html#Mr. Abdul Waheed Sheikh - welcoming Ambassador of UAE"> */}
+                                Mr. Abdul Waheed Sheikh - welcoming Ambassador of UAE
+                                {/*    </a>   */}
+                                <strong><i
                                     className="fa fa-calendar"></i>
                                     2019-06-20</strong></li>
                         </ul>
@@ -95,10 +108,12 @@ const Footer3a = () => {
                 </div>
 
                 <div className="footer-1-bar">
-                    <p>
-                        <a href="http://www.waheedgroup.com.pk/" target="_blank" rel="noopener noreferrer" style={{ color: "white" }} >
-                            Waheed Hafeez || Group of Companies
-                        </a> ©2023. All Rights Reserved.
+                    <p style={{ color: "white" }}>
+                        {/* <p> */}
+                        {/* <a href="http://www.waheedgroup.com.pk/" target="_blank" rel="noopener noreferrer" style={{ color: "white" }} > */}
+                        Waheed Hafeez || Group of Companies
+                        {/* </a> */}
+                        ©2023. All Rights Reserved.
                     </p>
                 </div>
 
